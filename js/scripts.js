7ed06207ddab00006f60c71bbcce607465050a7c
Joseph Karasek and Mary Warrington <marywarrington@gmail.com>
2016-01-11 14:21:31 -0800

    clean out bad code from js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,9 @@
 jQuery(document).ready(function(){
 
-  // $(".flashcard").click(function(){
-  //   $("#word").toggle();
-  //   $("#definition").toggle();
-  // })
-
   $(".flashcard").click(function(){
 
     $(this).find(".word").toggle();
     $(this).find(".description").toggle();
-
-    // $("this.word").toggle();
-    // $("this.definition").toggle();
 
   });
 });
