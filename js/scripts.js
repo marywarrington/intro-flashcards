1fed3ba8870eeb735346d4e2ad4a7596ddfcdd7b
Joseph Karasek and Mary Warrington <marywarrington@gmail.com>
2016-01-11 14:22:53 -0800

    Fix typo in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function(){
   $(".flashcard").click(function(){
 
     $(this).find(".word").toggle();
-    $(this).find(".description").toggle();
+    $(this).find(".definition").toggle();
 
   });
 });
